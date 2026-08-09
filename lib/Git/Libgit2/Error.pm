5f4405d357bf5c504c12128a784be2df03b55aa2
Torsten Raudssus <torsten@raudss.us>
2026-08-09 22:01:20 +0200

    Synchronize module versions for 0.005

--- lib/Git/Libgit2/Error.pm
+++ lib/Git/Libgit2/Error.pm
@@ -1,7 +1,7 @@
 # ABSTRACT: Wraps git_error_last() into a Perl structure
 
 package Git::Libgit2::Error;
-our $VERSION = '0.002';
+our $VERSION = '0.005';
 use strict;
 use warnings;
 use FFI::Platypus 2.00;
--- lib/Git/Libgit2/FFI.pm
+++ lib/Git/Libgit2/FFI.pm
@@ -1,7 +1,7 @@
 # ABSTRACT: Internal FFI::Platypus instance for Git::Libgit2
 
 package Git::Libgit2::FFI;
-our $VERSION = '0.002';
+our $VERSION = '0.005';
 use strict;
 use warnings;
 use FFI::Platypus 2.00;
